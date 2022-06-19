646cb2e37d08e1934bce582cc8ca97d12aa86c66
Henrique Bonini de Britto Menezes <henrique.b.britto@gmail.com>
2022-06-19 16:33:57 -0300

    Adding more methods to assignment 5's code

--- as5/as5/as5.py
+++ as5/as5/as5.py
@@ -40,13 +40,100 @@ def binarize_images(images, T):
         image[image >= T] = 1
     return images
 
+
 def erode_image(image):
-    pass
+    eroded = np.empty_like(image)
+    rows, cols = image.shape
+    for i in range(1, rows):
+        for j in range(1, cols):
+            min_val = np.min(image[i - 1 : i + 1, j - 1 : j + 1])
+            eroded[i, j] = min_val
+    return eroded
+
+
+def dilate_image(image):
+    dilated = np.empty_like(image)
+    rows, cols = image.shape
+    for i in range(1, rows):
+        for j in range(1, cols):
+            max_val = np.max(image[i - 1 : i + 1, j - 1 : j + 1])
+            dilated[i, j] = max_val
+    return dilated
 
 
 def erode_images(images):
-    eroded = [erode_image(img) for img in images]
+    eroded = list(map(erode_image, images))
     return eroded
+
+
+def dilate_images(images):
+    dilated = list(map(dilate_image, images))
+    return dilated
+
+
+def closing(images):
+    dilated = dilate_images(images)
+    closed = erode_images(dilated)
+    return closed
+
+
+def opening(images):
+    eroded = erode_images(images)
+    opened = dilate_images(eroded)
+    return opened
+
+
+def masks_from(gray_images, morphed_images):
+    masks = []
+    for gray, morphed in zip(gray_images, morphed_images):
+        mask1 = np.empty_like(gray)
+        mask2 = np.empty_like(gray)
+        mask1[morphed == 0] = gray[morphed == 0]
+        mask2[morphed == 1] = gray[morphed == 1]
+        masks.append((mask1, mask2))
+    return masks
+
+
+def cooccurrence_prob_matrix(mask, Q):
+    row, col = mask.shape
+    cooc_side = np.max(mask) + 1
+    cooc = np.zeros((cooc_side, cooc_side))
+    for i in range(1, row):
+        for j in range(1, col):
+            cooc[i, j][i + Q[0], j + Q[1]] += 1
+    cooc /= np.sum(cooc)
+    return cooc
+
+
+def cooccurrence_prob_matrixes(masks, Q):
+    matrixes = []
+    for mm in masks:
+        coocs = [cooccurrence_prob_matrix(m, Q) for m in mm]
+        matrixes.append(tuple(coocs))
+    return matrixes
+
+
+def compute_haralick_descriptors(cooccurrences):
+    descriptors = []
+    for coocs in cooccurrences:
+        auto_corr = [auto_correlation(cooc) for cooc in coocs]
+        contr = [contrast(cooc) for cooc in coocs]
+        diss = [dissimilarity(cooc) for cooc in coocs]
+        en = [energy(cooc) for cooc in coocs]
+        entr = [entropy(cooc) for cooc in coocs]
+        hom = [homogeneity(cooc) for cooc in coocs]
+        inv_diff = [inverse_difference(cooc) for cooc in coocs]
+        max_prob = [maximum_probability(cooc) for cooc in coocs]
+
+        # Transposing the tuples to have descriptors per co-occurrence matrix
+        descs = list(zip(*[auto_corr, contr, diss, en, entr, hom, inv_diff, max_prob]))
+        descs = np.concatenate((descs[0], descs[1]), axis=None)
+        descriptors.append(descs)
+    return descriptors
+
+
+def order_by_similarity():
+    pass
 
 
 def run():
@@ -60,6 +147,18 @@ def run():
     images = load_images(image_names)
     gray_images = convert_gray_luminance(images)
     bin_images = binarize_images(gray_images, T)
+    morphed_images = list()
+    if F == 1:
+        morphed_images = opening(bin_images)
+    elif F == 2:
+        morphed_images = closing(bin_images)
+
+    masks = masks_from(gray_images, morphed_images)
+
+    # part 2
+    cooc_matrixes = cooccurrence_prob_matrixes(masks)
+
+    sep_descriptors = compute_haralick_descriptors(cooc_matrixes)
 
 
 if __name__ == "__main__":
