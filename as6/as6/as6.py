ef9ebe637082f46af1618f5c549ca7d54e328560
Henrique Bonini de Britto Menezes <henrique.b.britto@gmail.com>
2022-07-06 11:44:55 -0300

    Optimizing assignment 6 code

--- as6/as6/as6.py
+++ as6/as6/as6.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import imageio
 import numpy as np
@@ -26,39 +26,35 @@ def distance(
 
 
 def find_closest_cluster(
-    feature: npt.NDArray[np.float32], centroids: npt.NDArray[np.float32]
-) -> Tuple[int, npt.NDArray[np.float32]]:
-    centr_ranks = [
-        (idx, distance(feature, centroid)) for idx, centroid in enumerate(centroids)
-    ]
-    centr_ranks.sort(key=lambda x: x[1])
-    return centr_ranks[0]
+    feature: npt.NDArray[np.float32], centroids: List[npt.NDArray[np.float32]]
+) -> int:
+    centr_ranks = [distance(feature, centroid) for centroid in centroids]
+    closest = np.argmin(centr_ranks)
+    return closest
 
 
 def k_means(
-    image: npt.NDArray[np.float32],
     features: npt.NDArray[np.float32],
     k: int,
     n: int,
     M: int,
     N: int,
     seed: int,
-) -> Dict[int, npt.NDArray[np.float32]]:
+) -> List[int]:
     random.seed(seed)
     ids = np.sort(random.sample(range(0, M * N), k))
     centroids = features[ids]
-    clustered = defaultdict(list)
-    loc_centroid = dict()
+    clusters = np.zeros(features.shape, dtype=np.uint8)
     for _ in range(n):
-        clustered.clear()
         for index, feature in enumerate(features):
-            closest_idx, _ = find_closest_cluster(feature, centroids)
-            clustered[closest_idx].append(feature)
-            loc_centroid[index] = centroids[closest_idx]
-        for idx, feats in clustered.items():
-            centroid = np.mean(feats)
-            centroids[idx] = centroid
-    return loc_centroid
+            closest_idx = find_closest_cluster(feature, centroids)
+            clusters[index] = closest_idx
+        for cluster in range(k):
+            centroids[cluster] = np.mean(features[np.where(clusters == cluster)], axis=0)
+    segmented = np.zeros_like(features)
+    for index in range(len(clusters)):
+        segmented[index] = centroids[clusters[index]]
+    return segmented
 
 
 def create_XY_features(M, N):
@@ -66,7 +62,7 @@ def create_XY_features(M, N):
     Y = np.tile(np.reshape(np.arange(N), (N, 1)), (M, 1))
     X = np.reshape(X, (M * N, 1))
     Y = np.reshape(Y, (M * N, 1))
-    XY = np.concatenate(X, Y, axis=1)
+    XY = np.concatenate((X, Y), axis=1)
     return XY
 
 
@@ -76,8 +72,19 @@ def lum_reshape(image, M, N):
         (M * N, 1),
     )
 
+def rgb_image_from(
+    clusters: List[int],
+    centroids: List[npt.NDArray[np.float32]],
+    feat_shape: Tuple,
+    img_shape: Tuple,
+):
+    out_image = np.zeros(feat_shape, dtype=np.float32)
+    out_image = centroids[clusters]
+    out_image = np.reshape(out_image, img_shape)
+    return out_image
 
-def image_from(clusters, feat_shape, img_shape):
+
+def gray_image_from(clusters, feat_shape, img_shape):
     out_image = np.zeros(feat_shape, dtype=np.float32)
     for index, centroid in clusters.items():
         out_image[index] = centroid
@@ -85,38 +92,47 @@ def image_from(clusters, feat_shape, img_shape):
     return out_image
 
 def main():
-    # BASE_PATH = '../tests/resources/TestCases-InputImages'
-    image_filename = str(input().strip())
-    ref_image_filename = str(input().strip())
-    attributes_type = int(input().strip())
-    k = int(input().strip())
-    n = int(input().strip())
-    seed = int(input().strip())
+    BASE_PATH = 'as6/tests/resources/TestCases-InputImages'
+    image_filename = os.path.join(BASE_PATH, 'image_1.png')
+    ref_image_filename = os.path.join(BASE_PATH, 'image_1_ref1.png')
+    attributes_type = 1
+    k = 5
+    n = 10
+    seed = 42 #nice
+    # image_filename = str(input().strip())
+    # ref_image_filename = str(input().strip())
+    # attributes_type = int(input().strip())
+    # k = int(input().strip())
+    # n = int(input().strip())
+    # seed = int(input().strip())
 
     image = imageio.imread(image_filename).astype(np.float32)
     ref_image = imageio.imread(ref_image_filename).astype(np.float32)
 
     M, N, _ = image.shape
-    rgb = 1
 
     if attributes_type == 1:
         features = np.reshape(image, (M*N, 3))
+        clusters = k_means(features, k, n, M, N, seed)
+        out_image = rgb_image_from(clusters, features.shape, (M, N, 3))
     elif attributes_type == 2:
         XY = create_XY_features(M, N)
         features = np.reshape(image, (M*N, 3))
-        features = np.concatenate(features, XY, axis=1)
+        features = np.concatenate((features, XY), axis=1)
+        clusters = k_means(features, k, n, M, N, seed)
+        out_image = rgb_image_from(clusters, features.shape, (M, N, 3))
     elif attributes_type == 3:
         features = lum_reshape(image, M, N)
-        rgb = 0
+        clusters = k_means(features, k, n, M, N, seed)
+        out_image = gray_image_from(clusters, features.shape, (M, N, 1))
     elif attributes_type == 4:
         features = lum_reshape(image, M, N)
-        features = np.concatenate(features, create_XY_features(M, N), axis=1)
-        rgb = 0
+        features = np.concatenate((features, create_XY_features(M, N)), axis=1)
+        clusters = k_means(features, k, n, M, N, seed)
+        out_image = gray_image_from(clusters, features.shape, (M, N, 1))
     else:
         exit(1)
 
-    clusters = k_means(image, features, k, n, M, N, seed)
-    out_image = image_from(clusters, features.shape, (M, N, 3) if rgb else (M, N, 1))
     rmse = root_mean_sq_err(ref_image, out_image)
     print(rmse)
 
